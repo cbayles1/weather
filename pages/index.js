d269212ed7e8f0097bd0ec60b1d45b5da1348da5
SuperNunb <clbayles@hotmail.com>
2022-12-24 16:44:01 -0600

    temperature output to page

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,13 @@ async function getLocation() {
   }
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps({params}) {
 
   let lat = 39.84031;
   let lon = -88.9548;
 
   const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_KEY}&exclude=minutely`);
   const data = await res.json();
-  if (data) {
-    console.log(data['current'].weather[0]['description']);
-  }
-
   return {
     props: {
       weatherData: data,
@@ -26,11 +22,12 @@ export async function getServerSideProps(context) {
   };
 }
 
-export default function Home() {
+export default function Home({weatherData}) {
 
   return (
     <div>
       <h1>MY WEATHER APP</h1>
+      <p>It is currently {weatherData['current'].temp} K where you live.</p>
     </div>
   );
 }
